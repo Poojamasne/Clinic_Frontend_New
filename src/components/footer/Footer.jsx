1b6cd54401d9f6244af76ef21c4e9f4d178e4446
Sanket Kadam <sanketkadam3921@gmail.com>
2025-12-10 17:54:52 +0530

    navbar and footer files updated

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -278,7 +278,7 @@ export default function Footer() {
           </Typography>
 
           <Box sx={{ display: "flex", gap: { xs: 2, sm: 3 } }}>
-            {["Privacy Policy", "Terms & Conditions"].map(
+            {["Privacy Policy", "Terms & Conditions", "Shipping Policy"].map(
               (text) => (
                 <MuiLink
                   key={text}
